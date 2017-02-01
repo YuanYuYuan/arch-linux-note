1fa3260d52837b6fd4928a2780d4896fe6d440ea
yuanyouyuan <az6980522@gmail.com>
2017-02-01 18:29:12 +0800

    update mac_arch.sh

--- mac_arch.sh
+++ mac_arch.sh
@@ -1,22 +1,24 @@
-echo -n "Enter the disk: "
-read device
+ #/dev/sda
 
-wipefs -a $device
-parted $device mklabel gpt
-parted $device mkpart ESP fat32 1M 513M
-parted $device set 1 boot on
-parted $device mkpart primary ext4 513M 100%
-parted $device print
+#parted $device mkpart ESP fat32 1M 513M
+#parted $device set 4 boot on
+#parted $device mkpart primary linux-swap 513M 100%
+#parted $device print
 
-mkfs.fat ${device}1
-mkfs.ext4 -O "^has_journal" ${device}2
+mkfs.fat /dev/sda4
+mkswap /dev/sda5
+swapon /dev/sda5
+mkfs.ext4 /dev/sda6
 
-mount ${device}2 /mnt
-mkdir -p /mnt/boot
-mount ${device}1 /mnt/boot
 
-pacstrap -i /mnt base base-devel
-genfstab -U /mnt >> /mnt/etc/fstab
+
+mount /dev/sda6 /mnt
+mkdir /mnt/boot
+mount /dev/sda4 /mnt/boot
+
+mv ranked_mirrolist /etc/pacman.d/mirrorlist
+pacstrap /mnt base base-devel
+genfstab -U -p /mnt >> /mnt/etc/fstab
 
 arch_chroot() {
     arch-chroot /mnt /bin/bash -c "${*}"
