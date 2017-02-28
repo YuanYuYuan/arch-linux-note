152bdf0048dd5a903a45bcc7a20eca1d5109d172
yuanyouyuan <az6980522@gmail.com>
2017-03-01 00:19:05 +0800

    update arch_on_mac_bootctl

--- installation_tools/arch_on_mac_bootctl/set_arch_on_mac_bootctl.sh
+++ installation_tools/arch_on_mac_bootctl/set_arch_on_mac_bootctl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-root_UUID=$(blkid -s PARTUUID -o value /dev/sd6)
+root_UUID=$(blkid -s PARTUUID -o value /dev/sda6)
 echo "options root=PARTUUID=$root_UUID rw" >> arch.conf
 cp ./loader.conf /boot/loader/loader.conf
 cp ./arch.conf /boot/loader/entries/loader.conf
