f47fba61784da44e400618303ef4ec4a3cf19e9e
yuanyuyuan <az6980522@gmail.com>
2021-10-05 12:11:36 +0800

    Add exit.sh

---
+++ scripts/exit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+lock() {
+    xset dpms force off
+    betterlockscreen --lock
+}
+
+output_sink="$(pactl list short sinks | grep output | head -n1 | cut -f2)"
+
+case "$1" in
+    lock)
+        lock
+        ;;
+    logout )
+        pkill -x Xorg
+        ;;
+    suspend)
+        pactl set-sink-mute $output_sink 1
+        systemctl suspend&
+        lock
+        pactl set-sink-mute $output_sink 0
+        ;;
+    reboot)
+        systemctl reboot
+        ;;
+    shutdown)
+        systemctl poweroff
+        ;;
+    *)
+        echo "Usage: $0 {lock|logout|suspend|reboot|shutdown}"
+esac
+exit 0
